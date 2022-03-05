61e23a610a489ca61a5b02bba0ddced711554253
dante01yoon <bunggl@naver.com>
2022-03-06 00:04:50 +0900

    update: build config - external dependencies, peerDependencies

--- build.js
+++ build.js
@@ -1,7 +1,12 @@
-const build = require("esbuild");
-
+const { build } = require("esbuild");
+const { dependencies, peerDependencies } = require("./package.json");
+console.log(dependencies, peerDependencies);
 build({
   entryPoints: ["src/index.ts"],
   outdir: "dist",
   bundle: true,
+  external: Object.keys({
+    ...dependencies,
+    ...peerDependencies,
+  }),
 });
