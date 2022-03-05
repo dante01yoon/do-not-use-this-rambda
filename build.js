903cc45a623d949a8fb4a96bc96a57aa0b81249b
dante01yoon <bunggl@naver.com>
2022-03-06 00:06:07 +0900

    chore: build.js console.log 삭제

--- build.js
+++ build.js
@@ -1,6 +1,6 @@
 const { build } = require("esbuild");
 const { dependencies, peerDependencies } = require("./package.json");
-console.log(dependencies, peerDependencies);
+
 build({
   entryPoints: ["src/index.ts"],
   outdir: "dist",
